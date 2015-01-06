e8f8fcb773a21d2ed5761519be7e2ec4cffdb491
kameron19921016 <kameron19921016@yahoo.com>
2015-01-06 08:23:47 +0800

    delete bootanimation
    Change-Id: Ic49fda2a24c5c366ea343e0c62fb1e09b1d9483c

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/CyanogenMod/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-11.0 | awk {'print $1'})
+for 
 do
     add_lunch_combo $combo
 done
