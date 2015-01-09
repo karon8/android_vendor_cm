6468c02e75e624db716574b290c19011f9eb8857
New One Android <kameron19921016@qq.com>
2015-01-09 13:50:01 +0800

    Update themes_common.mk

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -5,5 +5,5 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     vendor/one/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
-    vendor/one/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml \
+    vendor/one/config/permissions/org.one.theme.xml:system/etc/permissions/org.one.theme.xml \
     vendor/one/config/permissions/org.one.theme.xml:system/etc/permissions/org.one.theme.xml
