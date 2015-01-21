60671da0d3d068fea0a99a8de7f28c9fe1fd9b9d
karon <kameron19921016@yahoo.com>
2015-01-21 12:06:09 +0800

    vendor:use new link and changes
    Change-Id: Id939c14c6fbd5bffd01b19e301d96818659b8e7e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(cat vendor/one/one_build_targets)
+for combo in $(curl -s https://raw.githubusercontent.com/karon8/one_devices/master/one_build_targets | sed -e 's/#.*$//' | grep ONE50.2 | awk {'print $1'})
 do
     add_lunch_combo $combo
 done
